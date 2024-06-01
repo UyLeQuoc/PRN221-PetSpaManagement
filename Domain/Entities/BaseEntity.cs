da7a6e8867286e31537177c64e0969edde0459be
Le Quoc Uy <lequocuyit@gmail.com>
2024-06-01 13:05:42 +0700

    setup folder structure

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? ModificationDate { get; set; }
+
+        public int? ModificationBy { get; set; }
+
+        public DateTime? DeletionDate { get; set; }
+
+        public int? DeleteBy { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ Infrastructure/PetSpaManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class PetSpaManagementDbContext : DbContext
+    {
+        public PetSpaManagementDbContext(DbContextOptions<PetSpaManagementDbContext> options) : base(options)
+        {
+
+
+        }
+    }
+}
