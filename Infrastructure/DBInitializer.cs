ec10102b957666978f01765217383f86947ede2f
Le Quoc Uy <lequocuyit@gmail.com>
2024-07-12 00:28:28 +0700

    seed data role

---
+++ Infrastructure/DBInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure
+{
+	public static class DBInitializer
+	{
+		public static async Task Initialize(PetSpaManagementDbContext context)
+		{
+			if (!context.Roles.Any())
+			{
+				var roles = new List<Role>
+				{
+					new Role { Name = "Admin"},
+					new Role { Name = "Manager" },
+					new Role { Name = "PetSitter" },
+					new Role { Name = "Customer" }
+				};
+
+				foreach (var role in roles)
+				{
+					await context.Roles.AddAsync(role);
+				}
+				await context.SaveChangesAsync();
+			}
+		}
+	}
+}
--- PetSpaManagementWeb/Program.cs
+++ PetSpaManagementWeb/Program.cs
@@ -1,11 +1,9 @@
 using Application.Interfaces.IServices;
 using Application.Repositories;
 using Application.Services;
-using Domain.Entities;
 using Infrastructure;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,19 +11,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<PetSpaManagementDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB"));
 });
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IServiceService, ServiceService>();
 builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
 
+
+
 var app = builder.Build();
+
+// SCOPE FOR MIGRATION
+// explain: The CreateScope method creates a new scope. The scope is a way to manage the lifetime of objects in the container.
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<PetSpaManagementDbContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+	await DBInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+	logger.LogError(ex, "An problem occurred during migration!");
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+	app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
 
