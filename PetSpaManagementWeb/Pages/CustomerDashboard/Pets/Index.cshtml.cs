465dba96947e87197bc2487a59efb4b9f72dfbde
hoangtien1105 <minhtien11052003@gmail.com>
2024-07-16 15:46:40 +0700

    update get user id

--- PetSpaManagementWeb/Pages/CustomerDashboard/Pets/Index.cshtml.cs
+++ PetSpaManagementWeb/Pages/CustomerDashboard/Pets/Index.cshtml.cs
@@ -3,18 +3,22 @@ using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ServiceLayer.Interfaces;
+using ServiceLayer.Services;
 
 namespace PetSpaManagementWeb.Pages.CustomerDashboard.Pets
 {
     public class IndexModel : PageModel
     {
         private readonly IPetService _petService;
-        private readonly ILogger<EditModel> _logger; // Inject ILogger nếu cần
+        private readonly ILogger<EditModel> _logger;
+        private readonly IUserService _userService;
+        // Inject ILogger nếu cần
 
-        public IndexModel(IPetService petService, ILogger<EditModel> logger)
+        public IndexModel(IPetService petService, ILogger<EditModel> logger, IUserService userService)
         {
             _petService = petService;
             _logger = logger;
+            _userService = userService;
         }
 
         public IList<Pet> Pets { get; set; }
@@ -23,7 +27,16 @@ namespace PetSpaManagementWeb.Pages.CustomerDashboard.Pets
         {
             try
             {
-                Pets = await _petService.GetAllPets();
+                var email = HttpContext.Session.GetString("Email");
+
+                if (string.IsNullOrEmpty(email))
+                {
+                    TempData["ErrorMessage"] = "Please login to use this function"; // (Chỉ trong môi trường development)
+                    RedirectToPage("/LoginPage"); // Chuyển hướng về Index của Appointment
+                }
+                var user = await _userService.GetUserByEmailAsync(email);
+                var data = await _petService.GetAllPets();
+                Pets = data.Where(x => x.UserId == user.Id).ToList();
             }
             catch (Exception ex)
             {
